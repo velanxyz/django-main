d9dc68a786c3abc767bca302a478d60b67c9b739
velanz0r <ravilrustamov00777@gmail.com>
2023-12-13 00:44:22 +0300

    1

---
+++ mylocal/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def home(request):
+    return render(request, 'home.html')
+
+def FAQ(request):
+    return render(request, 'FAQ.html')
+
+def post(request):
+    posts = Post.objects.all()
+    return render(request, 'post.html', {'posts':posts})
